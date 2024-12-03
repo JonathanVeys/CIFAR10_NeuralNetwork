79225fb1dc49b0365fea51110e88cb5049ff34e9
Jonathan Veys <Jonathancr1234@gmail.com>
2024-12-03 22:53:45 +0000

    Created the model logic

--- CIFAR10_NN.py
+++ CIFAR10_NN.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader, random_split
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
+import torchvision.models as models
 import PIL
 
 import numpy as np
@@ -36,3 +37,28 @@ print(f'Valid dataset size = {len(valid_dataset)}')
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+class SimpleClassifier(nn.Module):
+    def __init__(self):
+        super(SimpleClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size = 3, padding = 1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 3, padding = 1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size = 3, padding = 1)
+
+        self.fc1 = nn.Linear(128*8*8, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 10)
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.pool(nn.ReLU(self.conv1(x)))
+        x = self.pool(nn.ReLU(self.conv2(x)))
+        x = self.pool(nn.ReLU(self.conv3(x)))
+        x = x.view(-1,128*8*8)
+        x = nn.ReLU(self.fc1(x))
+        x = self.dropout(x)
+        x = nn.ReLU(self.fc2(x))
+        x = self.fc3(x)
+        return(x)
